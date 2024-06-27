996eec61c70ecaa9ef2f24494b93feb9f29fcdfc
Haritha R <harithalaks1998@gmail.com>
2024-06-27 14:44:52 +0530

    Add project files.

---
+++ Logical Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true, b = false, x;
+
+            // AND operator
+            x = a && b;
+            Console.WriteLine("AND Operator: " + x);
+
+            // OR operator
+            x = a || b;
+            Console.WriteLine("OR Operator: " + x);
+
+            // NOT operator
+            x = !a;
+            Console.WriteLine("NOT Operator: " + x);
+             Console.ReadLine();
+        }
+    }
+}
